7419a4532866c31aad7c43e396f813c7e9f7188a
alisolano <100251377+alisolano@users.noreply.github.com>
2025-06-19 17:04:04 -0600

    version 1 con llvm generator

---
+++ Triangle/src/Triangle/CodeGenerator/LLVMGenerator.java
@@ -0,0 +1,379 @@
+package Triangle.CodeGenerator;
+
+import Triangle.AbstractSyntaxTrees.*;
+import Triangle.SyntacticAnalyzer.SourcePosition;
+
+public class LLVMGenerator implements Visitor {
+
+    private StringBuilder code;
+    private int tempCount;
+
+    public LLVMGenerator() {
+        code = new StringBuilder();
+        tempCount = 0;
+    }
+
+    public String generate(Program program) {
+        code.insert(0, "declare void @printInt(i32)\n"); // declaración externa
+        code.insert(0, "declare i32 @readInt()\n");
+        code.append("define i32 @main() {\n");
+        program.visit(this, null);
+        code.append("  ret i32 0\n");
+        code.append("}\n");
+        return code.toString();
+    }
+
+    private String newTemp() {
+        return "%t" + (tempCount++);
+    }
+
+    // Implementa los métodos visit... para cada nodo del AST que te interesa
+
+    public Object visitProgram(Program prog, Object o) {
+        prog.C.visit(this, o);
+        return null;
+    }
+
+    public Object visitIntegerLiteral(IntegerLiteral il, Object o) {
+        String temp = newTemp();
+        code.append("  ").append(temp).append(" = add i32 ").append(il.spelling).append(", 0\n");
+        return temp;
+    }
+
+    public Object visitVname(Vname vname, Object o) {
+        // Aquí deberás mapear nombres a LLVM variables (más adelante)
+        return null;
+    }
+
+    public Object visitAssignCommand(AssignCommand ac, Object o) {
+        // Ejemplo básico si tienes una variable n
+        String val = (String) ac.E.visit(this, o);
+        code.append("  store i32 ").append(val).append(", ptr @n\n");
+        return null;
+    }
+
+    // Agrega más visitas según sea necesario...
+
+    @Override
+    public Object visitCallCommand(CallCommand ast, Object o) {
+        if (ast.I.spelling.equals("print")) {
+            // Obtenemos el argumento de la llamada
+            String value = (String) ast.APS.visit(this, o);
+            code.append("  call void @printInt(i32 ").append(value).append(")\n");
+        }
+        return null;
+    }
+
+
+    @Override
+    public Object visitEmptyCommand(EmptyCommand ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitIfCommand(IfCommand ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitLetCommand(LetCommand lc, Object o) {
+        // Declaraciones: asumimos variables globales por ahora
+        lc.D.visit(this, o); // visita la declaración
+
+        // Comando dentro del 'in'
+        lc.C.visit(this, o); // visita el cuerpo del let
+        return null;
+    }
+
+
+    @Override
+    public Object visitSequentialCommand(SequentialCommand ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitWhileCommand(WhileCommand ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitArrayExpression(ArrayExpression ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitBinaryExpression(BinaryExpression ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+@Override
+public Object visitCallExpression(CallExpression ast, Object o) {
+    if (ast.I.spelling.equals("read")) {
+        String temp = newTemp();
+        code.append("  ").append(temp).append(" = call i32 @readInt()\n");
+        return temp;
+    }
+    return null;
+}
+
+
+
+    @Override
+    public Object visitCharacterExpression(CharacterExpression ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitEmptyExpression(EmptyExpression ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitIfExpression(IfExpression ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitIntegerExpression(IntegerExpression ast, Object o) {
+        return ast.IL.visit(this, o);  // IL es el IntegerLiteral
+    }
+
+
+    @Override
+    public Object visitLetExpression(LetExpression ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitRecordExpression(RecordExpression ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitUnaryExpression(UnaryExpression ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitVnameExpression(VnameExpression ast, Object o) {
+        return ast.V.visit(this, o);
+    }
+
+    @Override
+    public Object visitBinaryOperatorDeclaration(BinaryOperatorDeclaration ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitConstDeclaration(ConstDeclaration ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitFuncDeclaration(FuncDeclaration ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitProcDeclaration(ProcDeclaration ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitSequentialDeclaration(SequentialDeclaration ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitTypeDeclaration(TypeDeclaration ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitUnaryOperatorDeclaration(UnaryOperatorDeclaration ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+@Override
+public Object visitVarDeclaration(VarDeclaration ast, Object o) {
+    // Declaramos la variable como global, con valor inicial 0
+    String varName = ast.I.spelling;
+    code.insert(0, "@" + varName + " = global i32 0\n");
+    return null;
+}
+
+
+    @Override
+    public Object visitMultipleArrayAggregate(MultipleArrayAggregate ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitSingleArrayAggregate(SingleArrayAggregate ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitMultipleRecordAggregate(MultipleRecordAggregate ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitSingleRecordAggregate(SingleRecordAggregate ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitConstFormalParameter(ConstFormalParameter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitFuncFormalParameter(FuncFormalParameter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitProcFormalParameter(ProcFormalParameter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitVarFormalParameter(VarFormalParameter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitEmptyFormalParameterSequence(EmptyFormalParameterSequence ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitMultipleFormalParameterSequence(MultipleFormalParameterSequence ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitSingleFormalParameterSequence(SingleFormalParameterSequence ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitConstActualParameter(ConstActualParameter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitFuncActualParameter(FuncActualParameter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitProcActualParameter(ProcActualParameter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitVarActualParameter(VarActualParameter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitEmptyActualParameterSequence(EmptyActualParameterSequence ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitMultipleActualParameterSequence(MultipleActualParameterSequence ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitSingleActualParameterSequence(SingleActualParameterSequence ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitAnyTypeDenoter(AnyTypeDenoter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitArrayTypeDenoter(ArrayTypeDenoter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitBoolTypeDenoter(BoolTypeDenoter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitCharTypeDenoter(CharTypeDenoter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitErrorTypeDenoter(ErrorTypeDenoter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitSimpleTypeDenoter(SimpleTypeDenoter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitIntTypeDenoter(IntTypeDenoter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitRecordTypeDenoter(RecordTypeDenoter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitMultipleFieldTypeDenoter(MultipleFieldTypeDenoter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitSingleFieldTypeDenoter(SingleFieldTypeDenoter ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitCharacterLiteral(CharacterLiteral ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitIdentifier(Identifier ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitOperator(Operator ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitDotVname(DotVname ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object visitSimpleVname(SimpleVname ast, Object o) {
+        // Asumimos que es una variable global: %tmp = load i32, ptr @x
+        String temp = newTemp();
+        code.append("  ").append(temp).append(" = load i32, ptr @").append(ast.I.spelling).append("\n");
+        return temp;
+    }
+    @Override
+    public Object visitSubscriptVname(SubscriptVname ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
